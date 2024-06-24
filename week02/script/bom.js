ec24f1b4c8793b6b445a92dc4c0dc572cbe37a02
Andre Dos Reis <andredosreis@gmail.com>
2024-06-24 19:53:37 +0100

    finish part one
    finalizando o trabalhos da semana dois

---
+++ week02/script/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+//add event listener to button
+button.addEventListener("click", () =>  {
+    if (input.value != "") {
+        let chapter = input.value;
+        const li = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        li.textContent = chapter;
+        deleteBtn.textContent = " ❌";
+        deleteBtn.addEventListener("click", () => {
+            list.removeChild(li);
+        })
+        li.appendChild(deleteBtn);
+
+        list.appendChild(li);
+
+    }
+    input.value = "";
+    input.focus();
+
+    })

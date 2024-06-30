97f99608a2fc4ca6bb3b118ae898d707e2223704
Andre Dos Reis <andredosreis@gmail.com>
2024-06-30 02:17:36 +0100

    Dark mode

--- scripts/humburger.js
+++ scripts/humburger.js
@@ -5,3 +5,12 @@ humburgerElement.addEventListener('click', () => {
     navElement.classList.toggle('open');
     humburgerElement.classList.toggle('open');
 });
+
+//dark mode
+const darkModeButton = document.querySelector('#darkModeButton');
+const main = document.querySelector('main');
+
+darkModeButton.addEventListener('click', () => {
+    main.classList.toggle('dark');
+    darkModeButton.classList.toggle('dark');
+});

b94aa031d521e43cc61d4bbf30b63521b28eb255
Andre Dos Reis <andredosreis@gmail.com>
2024-07-10 20:18:25 +0100

    consertando o forms e fazendo o a temperatura

---
+++ scripts/weather.js
@@ -0,0 +1,4 @@
+const myTown = document.querySelector('#town')
+const myDescription = document.querySelector('#description')
+const myTemperature = document.querySelector('#temperature')
+const myGraphic = document.querySelector('#graphic')

664226043ea8f4dd3b029f91c67e813efca47d67
Andre Dos Reis <andredosreis@gmail.com>
2024-07-14 01:14:11 +0100

    alteração do link

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,7 @@ const links = "https://andredosreis.github.io/wdd230/data/links.json"
 async function getLinks() {
     const response = await fetch(links);
     const data = await response.json();
-    console.log(data);
-    //displayLinks(data.weeks);
+   // console.log(data);
+    displayLinks(data);
 }
 getLinks();

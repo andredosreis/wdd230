afe25f4eca0720e841aa71c781fec5d555b37e51
Andre Dos Reis <andredosreis@gmail.com>
2024-07-14 01:31:36 +0100

    corrigido erro JavaScript

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,19 @@
-const baseUrl = "https://andredosreis.github.io/wdd230/"
-const links = "https://andredosreis.github.io/wdd230/data/links.json"
+const baseUrl = "https://andredosreis.github.io/wdd230/";
+const links = "https://andredosreis.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(links);
     const data = await response.json();
-   // console.log(data);
     displayLinks(data);
 }
+
+function displayLinks(data) {
+    const weekLinks = document.querySelector("#week-links");
+    data.weeks.forEach(week => {
+        const weekItem = document.createElement("li");
+        weekItem.innerHTML = `${week.week}: ${week.links.map(link => `<a href="${baseUrl + link.url}">${link.title}</a>`).join(" | ")}`;
+        weekLinks.appendChild(weekItem);
+    });
+}
+
 getLinks();

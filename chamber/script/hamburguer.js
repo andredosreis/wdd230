c56df488ea228bc753de10068b2683ee39927812
Andre Dos Reis <63263157+andredosreis@users.noreply.github.com>
2024-07-25 04:47:45 +0000

    ajuste home css, colocando eventos

--- chamber/script/hamburguer.js
+++ chamber/script/hamburguer.js
@@ -6,3 +6,11 @@ humburgerElement.addEventListener('click', () => {
     humburgerElement.classList.toggle('open');
 });
 
+//dark mode
+const darkModeButton = document.querySelector('#darkModeButton');
+const main = document.querySelector('main');
+
+darkModeButton.addEventListener('click', () => {
+    main.classList.toggle('dark');
+    darkModeButton.classList.toggle('dark');
+});
